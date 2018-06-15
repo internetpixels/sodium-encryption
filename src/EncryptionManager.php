dbe6935f21acb7d6bc0b0dc459a31a0839c02253
Peter van Wilderen <7113270+Petervw@users.noreply.github.com>
2018-06-15 08:22:18 +0200

    Nonce length fix

--- src/EncryptionManager.php
+++ src/EncryptionManager.php
@@ -75,7 +75,7 @@ class EncryptionManager
      */
     public static function generateNonce()
     {
-        $nonce = random_bytes(64);
+        $nonce = random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES);
 
         return \sodium_bin2hex($nonce);
     }
